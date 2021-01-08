dcc23ac3bac2274b1a566e1e638ae1b73452f79e
UditThakkar <uditthacker1921@gmmail.com>
2021-01-08 20:05:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 var countdownDate = new Date("March 29, 2021 12:37:25").getTime();
 
 var x = setInterval(function () {
-    var now = new Date().getTime();
-    var distance = countdownDate - now;
+  var now = new Date().getTime();
+  var distance = countdownDate - now;
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("demo-days").innerHTML = days + "d ";
+  document.getElementById("demo-days").innerHTML = days + "d ";
 
 
   document.getElementById("demo-hours").innerHTML = hours + "h ";
 
 
-  document.getElementById("demo-min").innerHTML = minutes + "m " ;
+  document.getElementById("demo-min").innerHTML = minutes + "m ";
 
 
-  document.getElementById("demo-sec").innerHTML =  seconds + "s ";
+  document.getElementById("demo-sec").innerHTML = seconds + "s ";
 
 
 
@@ -32,4 +32,3 @@ var x = setInterval(function () {
 
   }
 }, 1000);
-    
